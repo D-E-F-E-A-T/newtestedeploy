8ceefc912685532695d58cf440817fb71f1bc4e6
shabazzBr <viniciuspoa2@hotmail.com>
2020-06-27 20:46:54 -0300

    Auto Push Atualizando Arquivos

--- cron.bat
+++ cron.bat
@@ -1,6 +1,6 @@
 @echo off
 
-start "%userprofile%\OneDrive - Processor\Área de Trabalho\newtestedeploy.git\"
+cd "%userprofile%\OneDrive - Processor\Área de Trabalho\newtestedeploy.git"
 push.cmd
 
 
