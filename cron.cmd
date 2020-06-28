14d1a2d7277ce7849262b21bac74af45f8d559bf
Vinicius  Pereira <viniciuspoa2@hotmail.com>
2020-06-28 15:17:34 -0300

    Update cron.cmd

--- cron.cmd
+++ cron.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 REM Foi necessário criar o arquivo push porque o agendador de tarefas executa no system32 e não dentro da pasta que eu precisava executar
-cd "C:\Users\vinicius.pereira\OneDrive - Processor\Área de Trabalho\newtestedeploy.git"
+cd "%userprofile%\OneDrive - Processor\Área de Trabalho\newtestedeploy.git" && push.cmd
 
 pause
