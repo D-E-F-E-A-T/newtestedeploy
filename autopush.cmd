e237dac911a7d84ce9620e15e4663c0c8e81fec0
shabazzBr <viniciuspoa2@hotmail.com>
2020-06-27 21:13:59 -0300

    Auto Push Atualizando Arquivos

--- autopush.cmd
+++ autopush.cmd
@@ -1,11 +1,11 @@
 @echo off
 
 
-schtasks /query /tn "GitAutoPush" && cls
+schtasks /query /tn "NewPush" && cls
 if %errorlevel% equ 0 (
     REM Ao Ver essa mensagem acesse seu agendador de terefas para alterar configurações
     REM Reexecutar o script vai sobreescrever as configurações
-    msg /w * Tarefa GitAutoPush sera executada ! && schtasks /run /tn "GitAutoPush"
+    msg /w * Tarefa GitAutoPush sera executada ! && schtasks /run /tn "NewPush"
     exit
 ) else (
     msg /w * Tarefa GitAutoPush ainda nao criada ! && goto criatarefa 
@@ -20,7 +20,9 @@ echo "O horario deve ser acertivo pois ira agendar assim que executado"
 echo.
 set /p hora=Horario^>
 REM Ao executar esse comando  o agendamento vai executar o script todos os dias nesse horário
-schtasks /create /sc daily /tn "GitAutoPush" /tr "%cd%\push.cmd" /st %hora%
+schtasks /create /sc daily /tn "NewPush" /tr "%cd%\cron.cmd" /st %hora%
 msg /w * Tarefa criada: %hora%
 
+
+REM "GitAutoPush" 
 pause
